9a0c2380d23d3a071bc2527c9873ab4ec9ebe0bb
Kenny Rakentine <specialistmorgenj@gmail.com>
2023-08-20 16:12:58 -0400

    responsive mediaQ for iPhone 12 Pro re-sizing

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -10,11 +10,11 @@ function GameOver() {
 
   return (
     <div className='gameOver'>
-        <h3>{gameOver.guessedWord ? "You Correctly Guessed" : "You Failed"}</h3>
-        <h1>The Correct Answer is "{correctWord}"</h1>
+        <h3 className='winState'>{gameOver.guessedWord ? "YOU CORRECTLY GUESSED" : "SORRY"}</h3>
+        <h1 className='correctTxt'> THE CORRECT ANSWER IS: "{correctWord.toUpperCase()}"</h1>
         {gameOver.guessedWord & (<h3> Guessed in {currAttempt.attempt} Attempts </h3>)}
         <br></br>
-        <button onClick={reload}>Try Again</button>
+        <button className='tryBtn' onClick={reload}>TRY AGAIN</button>
     </div>
   )
 }
